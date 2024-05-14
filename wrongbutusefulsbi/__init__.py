7675aca737d8b9417a708acc440ef748721c47b4
Ryan Kelly <ryan@kiiii.com>
2024-05-14 13:13:57 +1000

    Update __init__.py file

--- wrongbutusefulsbi/__init__.py
+++ wrongbutusefulsbi/__init__.py
@@ -4,8 +4,4 @@
 """TODO: PACKAGE DESCRIPTION
 """
 
-from wrongbutusefulsbi.examples.contaminated_normal import *
-from wrongbutusefulsbi.examples.contaminated_slcp import *
-from wrongbutusefulsbi.examples.misspec_ma1 import *
-from wrongbutusefulsbi.examples.sir import *
-from wrongbutusefulsbi.examples.toad import *
+from . import examples
--- wrongbutusefulsbi/examples/__init__.py
+++ wrongbutusefulsbi/examples/__init__.py
@@ -1,1 +1,8 @@
 # noqa: D104
+
+from .misspec_ma1 import *
+from .bvcbm import *
+from .contaminated_normal import *
+from .gaussian_vs_laplace import *
+from .sir import *
+from .wright_fisher import *
